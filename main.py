916fd4f6dd443426961ba0de32ceac8c5072ff30
Phil-Shepard <Philipp-74@yandex.ru>
2023-12-28 02:12:23 +0500

    Выводится окно достижений без текста

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 import Book
+from Achievement import Achivement
+
+achivements_list = [
+    "Completed 10 books",
+    "Reached 100 reading hours",
+    "Received Reader of the Month award"
+]
 
 Library = [Book.Book('0NchAwAAQBAJ'),Book.Book('fIyWTKDlMnAC'),Book.Book('86xhAwAAQBAJ'),
            Book.Book('0NchAwAAQBAJ'),Book.Book('fIyWTKDlMnAC'),Book.Book('86xhAwAAQBAJ')]
@@ -20,6 +27,7 @@ voice_button_img = PhotoImage(file="LibNote вёрстка/Плашка меню
 menu_logo = PhotoImage(file="LibNote вёрстка/Лого меню.png")
 my_library_bg_img = PhotoImage(file="LibNote вёрстка/Моя библиотека/Доп фон моя библиотека.png")
 recomendation_bg_img = PhotoImage(file="LibNote вёрстка/Рекомендации/Фон рекомендации.png")
+achivement_bg_img = PhotoImage(file="LibNote вёрстка/Group 52.png")
 #endregion
 
 background = ttk.Label(image=background_img)
@@ -29,6 +37,7 @@ background.place(x=0, y=0)
 def show_frame(frame):
        my_library_frame.place_forget()
        recomendation_frame.place_forget()
+       achivement_frame.place_forget()
        frame.place(x=328,y=47)
 
 #region Menu
@@ -45,7 +54,8 @@ Button(menu_frame,image=recomendation_button_img,border=0,
        command=lambda: show_frame(recomendation_frame)).place(x=133,y=293)
 
 Button(menu_frame,image=achivement_button_img,border=0,
-       bg="#C4AF72",activebackground="#C4AF72").place(x=133,y=466)
+       bg="#C4AF72",activebackground="#C4AF72",
+       command=lambda: show_frame(achivement_frame)).place(x=133,y=466)
 
 Button(menu_frame,image=voice_button_img,border=0,
        bg="#C4AF72",activebackground="#C4AF72").place(x=128,y=644)
@@ -68,6 +78,12 @@ else:
         truncated_text = f"{index}. {book.get_title()[:29]}..." if len(book.get_title()) > 29 else f"{index}. {book.get_title()}"
         ML_read_list.insert(END, truncated_text)
 
+# Создание экземпляра класса Achivement
+achivements = Achivement(*achivements_list)
+
+# Отображение списка достижений на achivement_frame
+# achivements.display_achivements(achivement_frame)
+
 #todo Добавить сроклл бар для библиотеки
 
 ML_read_list.place(x=99, y=76, width=449,height=160)
@@ -76,5 +92,8 @@ ML_read_list.place(x=99, y=76, width=449,height=160)
 
 recomendation_frame = Frame(width=696,height=675,border=0)
 ttk.Label(recomendation_frame, image=recomendation_bg_img,background='#DFD0B0').place(x=0, y=0)
+
+achivement_frame = Frame(width=696,height=675,border=0)
+ttk.Label(achivement_frame, image=achivement_bg_img,background='#DFD0B0').place(x=0, y=0)
 
 root.mainloop()
