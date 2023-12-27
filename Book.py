bd7a97e4495fec10f4c5e760e81d0894e8b762f8
AcryCxde <acry.wxrk@gmail.com>
2023-12-28 01:27:27 +0500

    Готов список книг

--- Book.py
+++ Book.py
@@ -1,42 +1,26 @@
 import requests
+from tkinter import *
+from tkinter import ttk
+
+
 API_KEY = 'AIzaSyAJ1j8sZsZS0ZE5JmrLIh1naLYgRMv7dgQ'
 class Book:
     all_info = None
-    status = 'WillRead'
-    mark = '-'
-    def __init__(self,id):
+    def __init__(self,id,mark='-',readCountOfPages='0'):
         self.id = id
+        self.mark = mark
+        self.readCountOfPages = readCountOfPages
         self.all_info = requests.get(f'https://www.googleapis.com/books/v1/volumes/{self.id}?=key{API_KEY}')
 
         if self.all_info.status_code == 200:
             self.all_info = self.all_info.json()
         else:
             print(f'Error: {self.all_info.status_code}')
-
-    def get_id(self):
-        return self.id
-
-    def get_all_info(self):
-        return self.all_info
-    def get_title(self):
-        return self.all_info['volumeInfo'].get("title",'Нет информации')
-
-    def get_authors(self):
-        return self.all_info['volumeInfo'].get('authors','Нет информации')
-
-    def get_categories(self):
-        return ''.join(self.all_info['volumeInfo'].get("categories",'Нет информации')).replace(" / General",'')
-
-    def get_pageCount(self):
-        return self.all_info['volumeInfo'].get("pageCount",'Нет информации')
-
-    def get_publishedDate(self):
-        return self.all_info['volumeInfo'].get("publishedDate",'Нет информации')
-
-    def get_description(self):
-        return self.all_info['volumeInfo'].get("description",'Нет информации')
-
-    def get_icon(self):
-        return self.all_info['volumeInfo']['imageLinks'].get("medium")
-
-
+        self.title = self.all_info['volumeInfo'].get("title",'Нет информации')
+        self.authors = ' '.join(self.all_info['volumeInfo'].get('authors','Нет информации'))
+        self.categories = (''.join(self.all_info['volumeInfo'].get("categories",'Нет информации'))
+                           .replace(" / General",''))
+        self.pageCount = self.all_info['volumeInfo'].get("pageCount",'Нет информации')
+        self.publishedDate = self.all_info['volumeInfo'].get("publishedDate",'Нет информации')
+        self.description = self.all_info['volumeInfo'].get("description",'Нет информации')
+        self.icon = self.all_info['volumeInfo']['imageLinks'].get("medium")
